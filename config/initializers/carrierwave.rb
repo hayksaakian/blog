3eeaf24052d75f477654a2ba2626fa222b8b4f20
hayksaakian <hayk.saakian@gmail.com>
2012-08-02 22:49:05 -0700

    finally

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -22,31 +22,31 @@ module CarrierWave
 
  # http://sleeplesscoding.blogspot.com/2011/09/recreate-single-version-of.html
  # Note: is_processing_delayed should be set before calling recreate_version! if the version depends on it.
- module Uploader
-   module Versions
-     def recreate_version!(version)
-       already_cached = cached?
-       cache_stored_file! if !already_cached
-       send(version).store!
-       if !already_cached && @cache_id
-         tmp_dir = File.expand_path(File.join(cache_dir, cache_id), CarrierWave.root)
-         FileUtils.rm_rf(tmp_dir)
-       end
-     end
-   end
- end
+ # module Uploader
+ #   module Versions
+ #     def recreate_version!(version)
+ #       already_cached = cached?
+ #       cache_stored_file! if !already_cached
+ #       send(version).store!
+ #       if !already_cached && @cache_id
+ #         tmp_dir = File.expand_path(File.join(cache_dir, cache_id), CarrierWave.root)
+ #         FileUtils.rm_rf(tmp_dir)
+ #       end
+ #     end
+ #   end
+ # end
  
- # Avoid overwriting original filename in delayed-processing.
- module Mount
-   class Mounter
-     def write_identifier
-       if remove?
-         record.write_uploader(serialization_column, '')
-       elsif uploader.identifier.present? &&
-         (!uploader.respond_to?(:is_processing_immediate?) || uploader.is <http://uploader.is>_processing_immediate?)
-         record.write_uploader(serialization_column, uploader.identifier)
-       end
-     end
-   end
- end
+ # # Avoid overwriting original filename in delayed-processing.
+ # module Mount
+ #   class Mounter
+ #     def write_identifier
+ #       if remove?
+ #         record.write_uploader(serialization_column, '')
+ #       elsif uploader.identifier.present? &&
+ #         (!uploader.respond_to?(:is_processing_immediate?) || uploader.is <http://uploader.is>_processing_immediate?)
+ #         record.write_uploader(serialization_column, uploader.identifier)
+ #       end
+ #     end
+ #   end
+ # end
 end
